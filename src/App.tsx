477266fb1eb9ec594482b77a4e67305b4f2a52e7
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2024-06-06 14:18:41 -0300

    build: bootstrap 5.3.3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
