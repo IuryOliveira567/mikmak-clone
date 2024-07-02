896095a8117b37bdb9fd344bb03a07b988257f08
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2024-07-02 15:33:05 -0300

    feat: add mikmak press panel

---
+++ src/components/InterCard.tsx
@@ -0,0 +1,23 @@
+function InterCard() {
+  return (
+    <div className="inter-card">
+      <div className="row">
+        <div className="col-md-6 video-pres">
+          <iframe className="video-fade-effect" src="https://www.youtube.com/embed/GrFG4Vopu5s?si=RaSDWVmwo_6UdxDE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        </div>
+        <div className="col-md-6 mikmak-pres">
+          <h1 className="slide-fade-effect">Why MikMak</h1>
+          <p className="slide-fade-effect">
+            We grow brands commerce-first, globally. Spanning 100<br/>
+            countries and 7000 Media & Retailer Partners Worldwide. Our<br/>
+            brand, retailer, and consumer insights will guide and impact<br/>
+            decision-making across your entire organization to drive real<br/>
+            business results.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InterCard;
